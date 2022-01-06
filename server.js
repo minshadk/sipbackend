96168aeec3272ea29670735d9a81d32c6820cf93
minshadk <minshadk13@gmail.com>
2022-01-07 00:10:59 +0530

    settig mongodb,mongoose

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = require("./app");
 
+dotenv.config({ path: "./config.env" });
 
-const app = require("./app")
+// const DB = process.env.URL.replace("<password>", process.env.URL);
+
+// console.log(process.env.URL)
+ 
+mongoose
+  .connect(process.env.URL)
+  .then(() => console.log("connected to MOngodb"))
+  .catch((err) => console.error("ITs an error from "));
 
 const port = process.env.PORT || 3001;
-app.listen(port,() => {
-    console.log(`App running on port ${3001}`)
-}) 
+app.listen(port, () => {
+  console.log(`App running on port ${3001}`);
+});
