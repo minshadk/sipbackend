cb6861529d96012569941e3f7821fa9c500fa12a
minshadk <minshadk13@gmail.com>
2022-03-18 23:15:14 +0530

    product order working

--- models/productOrderModel.js
+++ models/productOrderModel.js
@@ -1,18 +1,27 @@
 const mongoose = require("mongoose");
 
 const productOrderSchema = new mongoose.Schema({
-  productName: {
-    type: String,
-    required: [true, "A Product must have a name"],
-    unique: true,
-    trim: true
-  },
+  // productName: {
+  //   type: String,
+  //   required: [true, "A Product must have a name"],
+  //   unique: true,
+  //   trim: true
+  // },
 
   // productId: {
   //   type: mongoose.Schema.Types.ObjectId,
   //   required: true
   // },
-
+  product: {
+    productName: {
+      type: String,
+      required: true
+    },
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    }
+  },
   orderNumber: {
     type: Number,
     required: [true, "A Product must have a orderNumber"]
